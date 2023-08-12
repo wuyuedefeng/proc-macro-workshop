beb11a0c715994424a41146aac5828358beca651
sanmu <578595193@qq.com>
2023-08-12 19:28:37 +0800

    FEAT: finish builder/test-07

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,7 +2,7 @@ mod struct_generator;
 
 use proc_macro::TokenStream;
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let st = syn::parse_macro_input!(input as syn::DeriveInput);
     match struct_generator::generate(&st) {
--- builder/src/struct_generator/struct_fields_generator.rs
+++ builder/src/struct_generator/struct_fields_generator.rs
@@ -1,3 +1,7 @@
+use std::vec;
+
+use syn::spanned::Spanned;
+
 type StructFields = syn::punctuated::Punctuated<syn::Field, syn::Token![,]>;
 fn get_fields_from_derive_input(st: &syn::DeriveInput) -> syn::Result<&StructFields> {
     if let syn::Data::Struct(syn::DataStruct {
@@ -11,10 +15,10 @@ fn get_fields_from_derive_input(st: &syn::DeriveInput) -> syn::Result<&StructFie
     }
 }
 
-fn get_optional_inner_type(r#type: &syn::Type) -> Option<&syn::Type> {
+fn get_generic_inner_type<'a>(r#type: &'a syn::Type, outer_ident_name: &str) -> Option<&'a syn::Type> {
     if let syn::Type::Path(syn::TypePath { path: syn::Path { segments, .. }, .. }) = r#type {
         if let Some(seg) = segments.last() {
-            if seg.ident.to_string() == "Option" {
+            if seg.ident.to_string() == outer_ident_name {
                 if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments { args, .. }) = &seg.arguments {
                     if let Some(syn::GenericArgument::Type(inner_type)) = args.first() {
                         return Some(inner_type);
@@ -26,21 +30,55 @@ fn get_optional_inner_type(r#type: &syn::Type) -> Option<&syn::Type> {
     None
 }
 
+fn get_field_macro_attr_path_value(field: &syn::Field, attr_path: &str) -> Option<syn::Ident> {
+    let mut ident = None;
+    for attr in &field.attrs {
+        if let syn::Meta::List(list) = &attr.meta {
+            if let Some(p) = list.path.segments.first() {
+                if p.ident == "builder" {
+                    // let attr = attr.clone();
+                    let _ = list.parse_nested_meta(|nested_meta| {
+                        if nested_meta.path.is_ident(attr_path) {
+                            let expr: syn::Expr = nested_meta.value()?.parse()?;
+                            match expr {
+                                syn::Expr::Lit(expr) => {
+                                    if let syn::Lit::Str(ref ident_str) = expr.lit {
+                                        ident = Some(syn::Ident::new(ident_str.value().as_str(), field.span()));
+                                    }
+                                }
+                                _ => (),
+                            }
+                            Ok(())
+                        } else {
+                            Err(syn::Error::new(field.span(), "not support"))
+                        }
+                    });
+                }
+            }
+        }
+    }
+    ident
+}
+
 pub(crate) fn generate(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let fields = get_fields_from_derive_input(st)?;
 
     let idents: Vec<_> = fields.iter().map(|field| &field.ident).collect();
-    let types: Vec<_> = fields
-        .iter()
-        .map(|field| {
-            let r#type = &field.ty;
-            if let Some(inner_type) = get_optional_inner_type(r#type) {
-                quote::quote!(std::option::Option<#inner_type>)
+    let mut types = vec![];
+    for field in fields.iter() {
+        let r#type = &field.ty;
+        if let Some(inner_type) = get_generic_inner_type(r#type, "Option") {
+            types.push(quote::quote!(std::option::Option<#inner_type>))
+        } else if let Some(_) = get_field_macro_attr_path_value(field, "each") {
+            if let Some(_) = get_generic_inner_type(r#type, "Vec") {
+                types.push(quote::quote!(#r#type))
             } else {
-                quote::quote!(std::option::Option<#r#type>)
+                return Err(syn::Error::new(field.span(), "`each` field must be a Vec type"));
             }
-        })
-        .collect();
+        } else {
+            types.push(quote::quote!(std::option::Option<#r#type>))
+        }
+    }
 
     Ok(quote::quote!(
         #(#idents: #types),*
@@ -50,46 +88,75 @@ pub(crate) fn generate(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenS
 pub(crate) fn generate_builder_method_fields(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let fields = get_fields_from_derive_input(st)?;
 
-    let builder_clauses: Vec<_> = fields
-        .iter()
-        .map(|field| {
-            let ident = &field.ident;
-            quote::quote!(
-                #ident: std::option::Option::None
-            )
-        })
-        .collect();
+    let mut builder_clauses = vec![];
+    for field in fields.iter() {
+        let ident = &field.ident;
+        let r#type = &field.ty;
+        if let Some(_) = get_field_macro_attr_path_value(field, "each") {
+            if let Some(_) = get_generic_inner_type(r#type, "Vec") {
+                builder_clauses.push(quote::quote!(
+                    #ident: std::vec::Vec::new(),
+                ))
+            } else {
+                return Err(syn::Error::new(field.span(), "`each` field must be a Vec type"));
+            }
+        } else {
+            builder_clauses.push(quote::quote!(
+                #ident: std::option::Option::None,
+            ))
+        }
+    }
 
     Ok(quote::quote!(
-        #(#builder_clauses),*
+        #(#builder_clauses)*
     ))
 }
 
 pub(crate) fn generate_builder_setter_methods(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let fields = get_fields_from_derive_input(st)?;
 
-    let build_setter_methods: Vec<_> = fields
-        .iter()
-        .map(|field| {
-            let ident = &field.ident;
-            let r#type = &field.ty;
-            if let Some(inner_type) = get_optional_inner_type(r#type) {
-                quote::quote!(
-                    fn #ident(&mut self, #ident: #inner_type) -> &mut Self {
-                        self.#ident = std::option::Option::Some(#ident);
+    let mut build_setter_methods = vec![];
+    for field in fields.iter() {
+        let ident = &field.ident;
+        let r#type = &field.ty;
+
+        if let Some(inner_type) = get_generic_inner_type(r#type, "Option") {
+            build_setter_methods.push(quote::quote!(
+                fn #ident(&mut self, #ident: #inner_type) -> &mut Self {
+                    self.#ident = std::option::Option::Some(#ident);
+                    self
+                }
+            ))
+        } else if let Some(ref user_ident) = get_field_macro_attr_path_value(field, "each") {
+            if let Some(inner_type) = get_generic_inner_type(r#type, "Vec") {
+                let mut token_stream = proc_macro2::TokenStream::new();
+                token_stream.extend(quote::quote!(
+                    fn #user_ident(&mut self, #user_ident: #inner_type) -> &mut Self {
+                        self.#ident.push(#user_ident);
                         self
                     }
-                )
+                ));
+                if Some(user_ident) != ident.as_ref() {
+                    token_stream.extend(quote::quote!(
+                        fn #ident(&mut self, #ident: #r#type) -> &mut Self {
+                            self.#ident = #ident;
+                            self
+                        }
+                    ));
+                }
+                build_setter_methods.push(token_stream)
             } else {
-                quote::quote!(
-                    fn #ident(&mut self, #ident: #r#type) -> &mut Self {
-                        self.#ident = std::option::Option::Some(#ident);
-                        self
-                    }
-                )
+                return Err(syn::Error::new(field.span(), "`each` field must be a Vec type"));
             }
-        })
-        .collect();
+        } else {
+            build_setter_methods.push(quote::quote!(
+                fn #ident(&mut self, #ident: #r#type) -> &mut Self {
+                    self.#ident = std::option::Option::Some(#ident);
+                    self
+                }
+            ))
+        }
+    }
 
     Ok(quote::quote!(
         #(#build_setter_methods)*
@@ -104,7 +171,7 @@ pub(crate) fn generate_builder_build_method(st: &syn::DeriveInput) -> syn::Resul
         .filter_map(|field| {
             let ident = &field.ident;
             let r#type = &field.ty;
-            if let Some(_) = get_optional_inner_type(r#type) {
+            if get_generic_inner_type(r#type, "Option").is_some() || get_field_macro_attr_path_value(field, "each").is_some() {
                 None
             } else {
                 Some(quote::quote!(
@@ -123,7 +190,7 @@ pub(crate) fn generate_builder_build_method(st: &syn::DeriveInput) -> syn::Resul
             let ident = &field.ident;
             let r#type = &field.ty;
 
-            if let Some(_) = get_optional_inner_type(r#type) {
+            if get_generic_inner_type(r#type, "Option").is_some() || get_field_macro_attr_path_value(field, "each").is_some() {
                 quote::quote!(
                     #ident: self.#ident.clone(),
                 )

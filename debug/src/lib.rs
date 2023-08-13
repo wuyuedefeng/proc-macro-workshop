35e71b19a4838b06468db6b016b0c5be4226babd
sanmu <578595193@qq.com>
2023-08-13 15:11:06 +0800

    FEAT: finish debug/test-08

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -4,6 +4,7 @@ mod visitors;
 use std::vec;
 
 use proc_macro::TokenStream;
+use syn::parse::Parser;
 
 #[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -46,41 +47,47 @@ fn generate_debug_trait(st: &syn::DeriveInput) -> syn::Result<proc_macro2::Token
     let struct_name_ident = &st.ident;
     let debug_body_stream = generate_debug_trait_body(st)?;
 
-    let mut phantom_data_type_names = vec![];
-    let mut field_type_names = vec![];
-    let fields = common::get_fields_from_derive_input(st)?;
-    for field in fields.iter() {
-        if let Some(s) = common::get_phantom_data_generic_type_name(field)? {
-            phantom_data_type_names.push(s);
-        }
-        if let Some(s) = common::get_field_type_name(field)? {
-            field_type_names.push(s);
-        }
-    }
-    // 第七关
-    let associated_type_names = common::get_generic_associated_type(st);
-
     let mut generics = st.generics.clone();
-    for generic in generics.params.iter_mut() {
-        if let syn::GenericParam::Type(r#type) = generic {
-            let type_name = r#type.ident.to_string();
-            if phantom_data_type_names.contains(&type_name) && !field_type_names.contains(&type_name) {
-                continue;
+    // 第八关
+    if let Some(hatch) = get_struct_escape_hatch(st, "bound", Some(vec!["bound"]))? {
+        let where_clause = generics.make_where_clause();
+        where_clause.predicates.push(syn::parse_str(&hatch)?);
+    } else {
+        let mut phantom_data_type_names = vec![];
+        let mut field_type_names = vec![];
+        let fields = common::get_fields_from_derive_input(st)?;
+        for field in fields.iter() {
+            if let Some(s) = common::get_phantom_data_generic_type_name(field)? {
+                phantom_data_type_names.push(s);
             }
-            // 第七关
-            if associated_type_names.contains_key(&type_name) && !field_type_names.contains(&type_name) {
-                continue;
+            if let Some(s) = common::get_field_type_name(field)? {
+                field_type_names.push(s);
             }
-
-            r#type.bounds.push(syn::parse_quote!(std::fmt::Debug));
         }
-    }
+        // 第七关
+        let associated_type_names = common::get_generic_associated_type(st);
 
-    // 第七关
-    let where_clause = generics.make_where_clause();
-    for (_, associated_types) in associated_type_names {
-        for associated_type in associated_types {
-            where_clause.predicates.push(syn::parse_quote!(#associated_type: std::fmt::Debug));
+        for generic in generics.params.iter_mut() {
+            if let syn::GenericParam::Type(r#type) = generic {
+                let type_name = r#type.ident.to_string();
+                if phantom_data_type_names.contains(&type_name) && !field_type_names.contains(&type_name) {
+                    continue;
+                }
+                // 第七关
+                if associated_type_names.contains_key(&type_name) && !field_type_names.contains(&type_name) {
+                    continue;
+                }
+
+                r#type.bounds.push(syn::parse_quote!(std::fmt::Debug));
+            }
+        }
+
+        // 第七关
+        let where_clause = generics.make_where_clause();
+        for (_, associated_types) in associated_type_names {
+            for associated_type in associated_types {
+                where_clause.predicates.push(syn::parse_quote!(#associated_type: std::fmt::Debug));
+            }
         }
     }
 
@@ -93,4 +100,43 @@ fn generate_debug_trait(st: &syn::DeriveInput) -> syn::Result<proc_macro2::Token
             }
         }
     ))
+}
+
+// 第八关
+fn get_struct_escape_hatch(st: &syn::DeriveInput, attr_path: &str, allowed_outer_ident_names: Option<Vec<&str>>) -> syn::Result<Option<String>> {
+    for attr in &st.attrs {
+        if let syn::Meta::List(list) = &attr.meta {
+            if let Some(p) = list.path.segments.first() {
+                if p.ident == "debug" {
+                    let nested_metas = syn::punctuated::Punctuated::<syn::Meta, syn::Token![,]>::parse_terminated.parse2(list.tokens.clone()).unwrap();
+                    for nested_meta in nested_metas.iter() {
+                        if let syn::Meta::NameValue(kv) = nested_meta {
+                            if kv.path.is_ident(attr_path) {
+                                match &kv.value {
+                                    syn::Expr::Lit(expr) => {
+                                        if let syn::Lit::Str(ref ident_str) = expr.lit {
+                                            return Ok(Some(ident_str.value().to_string()));
+                                        }
+                                    }
+                                    _ => (),
+                                }
+                            }
+                            if let Some(ref allowed_outer_ident_names) = allowed_outer_ident_names {
+                                match kv.path.get_ident() {
+                                    Some(kv_path_ident) => {
+                                        let kv_path_name = kv_path_ident.to_string();
+                                        if allowed_outer_ident_names.iter().find(|allowed_name| *allowed_name == &kv_path_name).is_none() {
+                                            return Err(syn::Error::new_spanned(&list, format!(r#"expected `debug({} = "...")`"#, allowed_outer_ident_names.join("|"))));
+                                        }
+                                    }
+                                    _ => (),
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    Ok(None)
 }
--- main.rs
+++ main.rs
@@ -14,19 +14,24 @@ pub trait Trait {
 }
 
 #[derive(CustomDebug)]
-pub struct Field<T: Trait> {
+#[debug(bound = "T::Value: Debug")]
+pub struct Wrapper<T: Trait> {
+    field: Field<T>,
+}
+
+#[derive(CustomDebug)]
+struct Field<T: Trait> {
     values: Vec<T::Value>,
 }
 
 fn assert_debug<F: Debug>() {}
 
 fn main() {
-    // Does not implement Debug, but its associated type does.
     struct Id;
 
     impl Trait for Id {
         type Value = u8;
     }
 
-    assert_debug::<Field<Id>>();
+    assert_debug::<Wrapper<Id>>();
 }

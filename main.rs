c6c1ab1deb40f12a72f734b962b20b77658c5718
sanmu <578595193@qq.com>
2023-08-13 20:56:24 +0800

    FEAT: finish seq/test09

--- main.rs
+++ main.rs
@@ -8,18 +8,40 @@
 
 use seq::seq;
 
-seq!(N in 0..16 {
-    #[derive(Copy, Clone, PartialEq, Debug)]
-    enum Interrupt {
-        #(
-            Irq~N,
-        )*
-    }
-});
-
-fn main() {
-    let interrupt = Interrupt::Irq8;
-
-    assert_eq!(interrupt as u8, 8);
-    assert_eq!(interrupt, Interrupt::Irq8);
+// Source of truth. Call a given macro passing nproc as argument.
+//
+// We want this number to appear in only one place so that updating this one
+// number will correctly affect anything that depends on the number of procs.
+macro_rules! pass_nproc {
+    ($mac:ident) => {
+        $mac! { 256 }
+    };
 }
+
+macro_rules! literal_identity_macro {
+    ($nproc:literal) => {
+        $nproc
+    };
+}
+
+// Expands to: `const NPROC: usize = 256;`
+const NPROC: usize = pass_nproc!(literal_identity_macro);
+
+struct Proc;
+
+impl Proc {
+    const fn new() -> Self {
+        Proc
+    }
+}
+
+macro_rules! make_procs_array {
+    ($nproc:literal) => {
+        seq!(N in 0..$nproc { [#(Proc::new(),)*] })
+    }
+}
+
+// Expands to: `static PROCS: [Proc; NPROC] = [Proc::new(), ..., Proc::new()];`
+static PROCS: [Proc; NPROC] = pass_nproc!(make_procs_array);
+
+fn main() {}
--- seq/tests/progress.rs
+++ seq/tests/progress.rs
@@ -9,5 +9,5 @@ fn tests() {
     t.pass("tests/06-init-array.rs");
     t.pass("tests/07-inclusive-range.rs");
     t.compile_fail("tests/08-ident-span.rs");
-    //t.pass("tests/09-interaction-with-macrorules.rs");
+    t.pass("tests/09-interaction-with-macrorules.rs");
 }

7e544a55d38d39e7f863b0a9ff72931b792e5804
sanmu <578595193@qq.com>
2023-08-19 18:19:06 +0800

    FEAT: finish sorted/test01-04

--- main.rs
+++ main.rs
@@ -6,42 +6,21 @@
 // To run the code:
 //     $ cargo run
 
-use seq::seq;
+use sorted::sorted;
 
-// Source of truth. Call a given macro passing nproc as argument.
-//
-// We want this number to appear in only one place so that updating this one
-// number will correctly affect anything that depends on the number of procs.
-macro_rules! pass_nproc {
-    ($mac:ident) => {
-        $mac! { 256 }
-    };
+use std::env::VarError;
+use std::error::Error as StdError;
+use std::fmt;
+use std::io;
+use std::str::Utf8Error;
+
+#[sorted]
+pub enum Error {
+    Fmt(fmt::Error),
+    Io(io::Error),
+    Utf8(Utf8Error),
+    Var(VarError),
+    Dyn(Box<dyn StdError>),
 }
-
-macro_rules! literal_identity_macro {
-    ($nproc:literal) => {
-        $nproc
-    };
-}
-
-// Expands to: `const NPROC: usize = 256;`
-const NPROC: usize = pass_nproc!(literal_identity_macro);
-
-struct Proc;
-
-impl Proc {
-    const fn new() -> Self {
-        Proc
-    }
-}
-
-macro_rules! make_procs_array {
-    ($nproc:literal) => {
-        seq!(N in 0..$nproc { [#(Proc::new(),)*] })
-    }
-}
-
-// Expands to: `static PROCS: [Proc; NPROC] = [Proc::new(), ..., Proc::new()];`
-static PROCS: [Proc; NPROC] = pass_nproc!(make_procs_array);
 
 fn main() {}
--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,38 @@
 use proc_macro::TokenStream;
+use quote::ToTokens;
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
     let _ = input;
 
-    unimplemented!()
+    let st = syn::parse_macro_input!(input as syn::Item);
+    match do_expand(&st) {
+        Ok(token_stream) => token_stream.into(),
+        Err(e) => {
+            let mut t = e.to_compile_error();
+            t.extend(st.to_token_stream());
+            t.into()
+        }
+    }
+}
+
+fn do_expand(st: &syn::Item) -> syn::Result<proc_macro2::TokenStream> {
+    match st {
+        syn::Item::Enum(e) => check_enum_order(e),
+        _ => syn::Result::Err(syn::Error::new(proc_macro2::Span::call_site(), "expected enum or match expression")),
+    }
+}
+
+fn check_enum_order(st: &syn::ItemEnum) -> syn::Result<proc_macro2::TokenStream> {
+    let origin_order: Vec<_> = st.variants.iter().map(|f| (f.ident.to_string(), f)).collect();
+    let mut sorted = origin_order.clone();
+    sorted.sort_by(|a, b| a.0.cmp(&b.0));
+    for (a, b) in origin_order.iter().zip(sorted) {
+        if a.0 != b.0 {
+            return syn::Result::Err(syn::Error::new_spanned(&b.1.ident, format!("{} should sort before {}", b.0, a.0)));
+        }
+    }
+
+    Ok(st.into_token_stream())
 }
